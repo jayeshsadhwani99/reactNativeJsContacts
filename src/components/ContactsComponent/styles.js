15bbbe3f7fec93419bd30d1c4d21d4ed6e751177
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-15 14:06:45 +0530

    Style flat list

--- src/components/ContactsComponent/styles.js
+++ src/components/ContactsComponent/styles.js
@@ -1,14 +1,25 @@
 import {StyleSheet} from 'react-native';
+import colors from '../../assets/theme/colors';
 
 export default StyleSheet.create({
   itemContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingRight: 20,
+    alignItems: 'center',
   },
   item: {
     flexDirection: 'row',
     paddingVertical: 10,
+    alignItems: 'center',
     paddingHorizontal: 20,
+  },
+  name: {
+    fontSize: 17,
+  },
+  phoneNumber: {
+    opacity: 0.6,
+    fontSize: 14,
+    paddingVertical: 5,
   },
 });
