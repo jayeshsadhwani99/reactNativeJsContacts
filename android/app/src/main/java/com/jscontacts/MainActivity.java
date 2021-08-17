321a2bc0848213c84f78eafb375daa31cf37dfeb
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-17 20:38:21 +0530

    Change package and app name

--- android/app/src/main/java/com/jscontacts/MainActivity.java
+++ android/app/src/main/java/com/jscontacts/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jscontacts;
+package com.jayesh.jscontacts;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/jscontacts/MainApplication.java
+++ android/app/src/main/java/com/jscontacts/MainApplication.java
@@ -1,4 +1,4 @@
-package com.jscontacts;
+package com.jayesh.jscontacts;
 import com.facebook.react.bridge.JSIModulePackage; // <- add
   import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
@@ -71,7 +71,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.jscontacts.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.jayesh.jscontacts.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
