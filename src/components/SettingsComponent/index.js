1565d81b472a4b71672cc644de5bda8d35dd0304
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-17 20:51:42 +0530

    Create settings component

---
+++ src/components/SettingsComponent/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
+import Container from '../common/Container';
+import colors from '../../assets/theme/colors';
+
+const SettingsComponent = ({settingsOptions}) => {
+  return (
+    <ScrollView style={{backgroundColor: colors.white}}>
+      {settingsOptions.map(({title, subTitle, onPress}, index) => (
+        <TouchableOpacity key={title}>
+          <View
+            style={{
+              paddingHorizontal: 20,
+              paddingBottom: 20,
+              paddingTop: 20,
+            }}>
+            <Text style={{fontSize: 17}}>{title}</Text>
+            {subTitle && (
+              <Text style={{fontSize: 14, opacity: 0.5, paddingTop: 5}}>
+                {subTitle}
+              </Text>
+            )}
+          </View>
+
+          <View style={{height: 0.5, backgroundColor: colors.grey}} />
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default SettingsComponent;
