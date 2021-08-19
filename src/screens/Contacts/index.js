81e5051b6b90a7f2339c8b4520fcd5a5713b0579
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-08-19 13:17:23 +0530

    Component did update

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,28 +1,59 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
-import React, {useContext} from 'react';
-import {TouchableOpacity} from 'react-native';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
 import Icon from '../../components/common/Icon';
 import ContactsComponent from '../../components/ContactsComponent';
+import {CONTACT_DETAIL} from '../../constants/routeNames';
 import getContacts from '../../context/actions/contacts/getContacts';
 import {GlobalContext} from '../../context/Provider';
+import {navigate} from '../../navigations/SideMenu/RootNavigator';
 
-const Contacts = () => {
-  const [sortBy, setSortBy] = React.useState();
+const Contacts = ({navigation}) => {
+  const [sortBy, setSortBy] = React.useState(null);
   const {setOptions, toggleDrawer} = useNavigation();
-  const [modalVisible, setModalVisible] = React.useState(false);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  const contactsRef = useRef([]);
   const {
     contactsDispatch,
     contactsState: {
-      getContacts: {data, loading},
+      getContacts: {data, loading, error},
     },
   } = useContext(GlobalContext);
 
-  React.useEffect(() => {
+  useEffect(() => {
     getContacts()(contactsDispatch);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const getSettings = async () => {
+    const sortPref = await AsyncStorage.getItem('sortBy');
+    if (sortPref) {
+      setSortBy(sortPref);
+    }
+  };
+
+  useFocusEffect(
+    React.useCallback(() => {
+      getSettings();
+      return () => {};
+    }, []),
+  );
+
+  useEffect(() => {
+    const prev = contactsRef.current;
+
+    contactsRef.current = data;
+
+    const newList = contactsRef.current;
+    if (newList.length - prev.length === 1) {
+      const newContact = newList.find(
+        item => !prev.map(i => i.id).includes(item.id),
+      );
+      navigate(CONTACT_DETAIL, {item: newContact});
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data.length]);
 
   React.useEffect(() => {
     setOptions({
@@ -31,28 +62,12 @@ const Contacts = () => {
           onPress={() => {
             toggleDrawer();
           }}>
-          <Icon type="material" size={25} style={{padding: 10}} name="menu" />
+          <Icon type="material" style={{padding: 10}} size={25} name="menu" />
         </TouchableOpacity>
       ),
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const getSettings = async () => {
-    const sortPref = await AsyncStorage.getItem('sortBy');
-    if (sortBy) {
-      setSortBy(sortPref);
-    }
-  };
-
-  useFocusEffect(
-    React.useCallback(() => {
-      getSettings();
-
-      return () => {};
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []),
-  );
 
   return (
     <ContactsComponent
